a361c9c792deb1588077f246ced424018b5c8ee5
Christoph Wildhagen <git@christoph-wildhagen.de>
2019-01-27 23:14:03 +0100

    Changed to AndroidX and tryed to create xml settings

---
+++ app/src/main/java/org/asdfgamer/sunriseClock/ui/settings/SettingsViewModel.java
@@ -0,0 +1,7 @@
+package org.asdfgamer.sunriseClock.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
