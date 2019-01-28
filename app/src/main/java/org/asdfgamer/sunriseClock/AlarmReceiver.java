fee662ec829ee294c8c0d2a163b074386fc8694b
Christoph Wildhagen <git@christoph-wildhagen.de>
2019-01-28 22:54:55 +0100

    Removed many warnings, translated strings

--- app/src/main/java/org/asdfgamer/sunriseClock/AlarmReceiver.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/AlarmReceiver.java
@@ -6,13 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 import java.util.Objects;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -22,7 +15,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         {
             Log.w(context.getString(R.string.app_name),"The received Broadcast had the wrong action: " + intent.getAction());
             return;
-        };
+        }
         AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         if (alarm == null || alarm.getNextAlarmClock() == null) {
             return;
--- app/src/main/java/org/asdfgamer/sunriseClock/PreferencesActivity.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/PreferencesActivity.java
@@ -17,8 +17,9 @@ public class PreferencesActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
-
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
