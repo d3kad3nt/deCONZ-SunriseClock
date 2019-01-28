ffcf7bcd093d68cab9fd9fcdd68f3143675865d9
Christoph <wildhagenc@hotmail.de>
2019-01-28 20:26:35 +0100

    Added Activity for settings

--- app/src/main/java/org/asdfgamer/sunriseClock/ui/settings/SettingsViewModel.java
+++
@@ -1,7 +0,0 @@
-package org.asdfgamer.sunriseClock.ui.settings;
-
-import androidx.lifecycle.ViewModel;
-
-public class SettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
