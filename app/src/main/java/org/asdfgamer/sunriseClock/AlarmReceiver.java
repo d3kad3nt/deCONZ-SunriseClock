3ab7cbbb17a81315dabb997435d986102f1536b3
Kai-Uwe Lankenau <github@familie-lankenau.de>
2019-02-11 20:50:25 +0100

    Added prerequisite check for AlarmReceiver, checks if WiFi is connected.

--- app/src/main/java/org/asdfgamer/sunriseClock/AlarmReceiver.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/AlarmReceiver.java
@@ -4,39 +4,70 @@ import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 import java.util.Objects;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    private String TAG = "AlarmReceiver";
+
+    private Context context;
+
     @Override
     public void onReceive(final Context context, final Intent intent) {
-        if (!Objects.equals(intent.getAction(), "android.app.action.NEXT_ALARM_CLOCK_CHANGED"))
-        {
-            Log.w(context.getString(R.string.app_name),"The received Broadcast had the wrong action: " + intent.getAction());
+        Log.i(TAG, "Received intent.");
+
+        this.context = context;
+
+        if (!Objects.equals(intent.getAction(), "android.app.action.NEXT_ALARM_CLOCK_CHANGED")) {
+            Log.w(context.getString(R.string.app_name), "The received Broadcast had the wrong action: " + intent.getAction());
             return;
         }
-        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarm = (AlarmManager) this.context.getSystemService(Context.ALARM_SERVICE);
         if (alarm == null || alarm.getNextAlarmClock() == null) {
             return;
         }
 
         removeObsoleteSchedules();
-        addSchedule(alarm, context);
+        if (checkPrerequisites()) {
+            addSchedule(alarm, context);
+        }
+
     }
 
     /* TODO: Retrieve formerly used schedule ids (probably from local storage) to remove obsolete schedulues from deconz.
-    * Could use the new WorkManager from jetpack. */
+     * Could use the new WorkManager from jetpack. */
     private void removeObsoleteSchedules() {
 
     }
 
     private void addSchedule(AlarmManager alarm, Context context) {
-        //TODO: Only execute if in defined wifi connection (TODO: settings element).
         final PendingResult pendingResult = goAsync();
         SchedulingTask asyncTask = new SchedulingTask(pendingResult, alarm, context);
         asyncTask.execute();
+    }
+
+    /**
+     * Checks whether WiFi is enabled and device is connected to to a specific BSSID (from settings).
+     *
+     * @return True for device states matching the prerequisites.
+     */
+    private boolean checkPrerequisites() {
+        WifiManager wifiManager = (WifiManager) this.context.getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            String bssid = wifiInfo.getBSSID();
+            Log.d(TAG, "Currently connected to BSSID: " + bssid);
+            //TODO: Only execute if in defined wifi connection (TODO: settings element).
+            return true;
+        } else {
+            Log.d(TAG, "WiFi is not enabled.");
+            return false;
+        }
+
     }
 
 }
