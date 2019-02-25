2477f77f929bc1b6ecca72eb0703f229cdef4b2e
Kai-Uwe Lankenau <github@familie-lankenau.de>
2019-02-25 21:45:36 +0100

    Added main tabs for overview, lights, time. Lights tab with demo data. Preparation for usage with real light data from deconz.

--- app/src/main/java/org/asdfgamer/sunriseClock/maintabs/DeconzPagerAdapter.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/maintabs/DeconzPagerAdapter.java
@@ -1,16 +1,22 @@
 package org.asdfgamer.sunriseClock.maintabs;
 
+import android.content.Context;
+
+import org.asdfgamer.sunriseClock.R;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class DeconzPagerAdapter extends FragmentPagerAdapter {
-    private static int NUM_ITEMS = 4;
-    private static String[] TITLES = new String[]{"Overview", "Lights", "Groups", "Time"};
+    private static int NUM_ITEMS = 3;
 
-    public DeconzPagerAdapter(FragmentManager fragmentManager) {
+    private Context context;
+
+    public DeconzPagerAdapter(FragmentManager fragmentManager, Context context) {
         super(fragmentManager);
+        this.context = context;
     }
 
     // Returns total number of pages
@@ -20,26 +26,34 @@ public class DeconzPagerAdapter extends FragmentPagerAdapter {
     }
 
     // Returns the fragment to display for that page
+    @NonNull
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                return OverviewFragment.newInstance(0, TITLES[position]);
+                return OverviewFragment.newInstance(0, context.getString(R.string.tab_overview));
             case 1:
-                return LightsFragment.newInstance(1, TITLES[position]);
+                return LightsFragment.newInstance(1, context.getString(R.string.tab_lights));
             case 2:
-                return GroupsFragment.newInstance(2, TITLES[position]);
-            case 3:
-                return TimeFragment.newInstance(3, TITLES[position]);
+                return TimeFragment.newInstance(3, context.getString(R.string.tab_time));
             default:
-                return null;
+                return OverviewFragment.newInstance(0, context.getString(R.string.tab_overview));
         }
     }
 
     // Returns the page title for the top indicator
     @Override
     public CharSequence getPageTitle(int position) {
-        return TITLES[position];
+        switch (position) {
+            case 0:
+                return context.getString(R.string.tab_overview);
+            case 1:
+                return context.getString(R.string.tab_lights);
+            case 2:
+                return context.getString(R.string.tab_time);
+            default:
+                return null;
+        }
     }
 
 }
