47888bd50f6550ff8dc006289c5c77178c03ee91
Christoph Wildhagen <git@christoph-wildhagen.de>
2019-02-25 23:34:45 +0100

    Fixed Bug that bare SuccessListener don't call the GUIListener

--- app/src/main/java/org/asdfgamer/sunriseClock/network/listener/SuccessListener.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/network/listener/SuccessListener.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.android.volley.Response;
 
+import org.asdfgamer.sunriseClock.network.response.DeconzResponse;
 import org.json.JSONObject;
 
 public abstract class SuccessListener implements Response.Listener<JSONObject> {
@@ -14,18 +15,25 @@ public abstract class SuccessListener implements Response.Listener<JSONObject> {
      * The Listener that comes from the gui and returns the results to it.
      */
     private final GUIListener guiListener;
+    private final DeconzResponse deconzResponse;
 
 
-    SuccessListener(GUIListener guiListener) {
+    SuccessListener(GUIListener guiListener, DeconzResponse respose) {
         this.guiListener = guiListener;
+        this.deconzResponse = respose;
     }
 
     @Override
     public void onResponse(JSONObject response) {
         Log.i(TAG, "The request was an Success");
+        getGuiListener().callback(deconzResponse);
     }
 
     GUIListener getGuiListener() {
         return guiListener;
+    }
+
+    public DeconzResponse getDeconzResponse() {
+        return deconzResponse;
     }
 }
