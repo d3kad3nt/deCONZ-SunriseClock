e80482ca6ebe7498e88af001737f64d0b35f404c
Christoph <wildhagenc@hotmail.de>
2019-03-17 21:53:34 +0100

    Added default values to Settings and added Settings that no schedules get created
    It is now possible to deactivate the sheduling of alarms.
    Added default values, because the app crashed after it was deinstalled.

--- app/src/main/java/org/asdfgamer/sunriseClock/preferences/AlarmFragment.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/preferences/AlarmFragment.java
@@ -1,5 +1,6 @@
 package org.asdfgamer.sunriseClock.preferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -7,12 +8,35 @@ import org.asdfgamer.sunriseClock.R;
 
 import androidx.preference.PreferenceFragmentCompat;
 
-public class AlarmFragment extends PreferenceFragmentCompat {
+public class AlarmFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String TAG = "AlarmFragment";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         Log.i("sunriseClock", "Settings");
         setPreferencesFromResource(R.xml.preferences_alarm, rootKey);
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.d(TAG, key + " changed.");
     }
 }
