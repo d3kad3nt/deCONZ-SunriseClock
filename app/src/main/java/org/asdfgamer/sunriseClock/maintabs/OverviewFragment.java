1f86af0ee447baa945cf4567c848d9c8b70e6b83
Christoph Wildhagen <git@christoph-wildhagen.de>
2019-07-06 01:13:14 +0200

    Show alarm time or a greyed out clock with time 00:00. This added also an Utility Class for easy access to time management.

--- app/src/main/java/org/asdfgamer/sunriseClock/maintabs/OverviewFragment.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/maintabs/OverviewFragment.java
@@ -1,5 +1,6 @@
 package org.asdfgamer.sunriseClock.maintabs;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
 
 import org.asdfgamer.sunriseClock.R;
+import org.asdfgamer.sunriseClock.utils.SettingKeys;
+import org.asdfgamer.sunriseClock.utils.SimpleTime;
+
+import java.util.Objects;
 
 public class OverviewFragment extends Fragment {
 
@@ -35,9 +41,30 @@ public class OverviewFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_overview, container, false);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(Objects.requireNonNull(getActivity()));
+        long alarm = preferences.getLong(SettingKeys.ALARM_TIME.toString(), 0);
+        if (alarm != 0) {
+            showClock(view, alarm);
+        } else {
+            showInfo(view);
+        }
+
+        return view;
+    }
+
+    private void showInfo(View view) {
+        CustomAnalogClock customAnalogClock = view.findViewById(R.id.analog_clock);
+        customAnalogClock.setFace(R.drawable.ic_clock_face_grey);
+        customAnalogClock.setScale(2);
+        long standardTime = new SimpleTime(0, 0).getUnixTime();
+        customAnalogClock.setTime(standardTime);
+    }
+
+    private void showClock(@NonNull View view, long alarm) {
         CustomAnalogClock customAnalogClock = view.findViewById(R.id.analog_clock);
         customAnalogClock.setFace(R.drawable.ic_clock_face_green);
         customAnalogClock.setScale(2);
-        return view;
+        customAnalogClock.setTime(alarm);
     }
 }
