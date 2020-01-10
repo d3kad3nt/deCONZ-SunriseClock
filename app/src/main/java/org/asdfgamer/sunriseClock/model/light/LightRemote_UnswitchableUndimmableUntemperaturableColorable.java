608a20cbe047eed0723803318b4076fe3c5a9bba
Kai-Uwe Lankenau <kai-uwe.lankenau@familie-lankenau.de>
2020-01-10 16:23:07 +0100

    Cleanup.

--- app/src/main/java/org/asdfgamer/sunriseClock/model/light/LightRemote_UnswitchableUndimmableUntemperaturableColorable.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/model/light/LightRemote_UnswitchableUndimmableUntemperaturableColorable.java
@@ -7,8 +7,6 @@ public class LightRemote_UnswitchableUndimmableUntemperaturableColorable extends
 
     static final String TABLENAME = "light_unswitchable_undimmable_untemperaturable_colorable";
 
-    public static ILightBaseDao<LightRemote_UnswitchableUndimmableUntemperaturableColorable> dao;
-
     public LightRemote_UnswitchableUndimmableUntemperaturableColorable(int id, String friendlyName, int color) {
         super(id, friendlyName);
         super.setColor(color);
