912033d49c8c820fd8f3c904da3cb204b87f041c
Christoph <git@christoph-wildhagen.de>
2020-02-27 15:15:11 +0100

    Added Enpointmanager and created empty DeconzEndpointBuilder

---
+++ app/src/main/java/org/asdfgamer/sunriseClock/model/endpoint/EndpointManager.java
@@ -0,0 +1,55 @@
+package org.asdfgamer.sunriseClock.model.endpoint;
+
+import android.content.Context;
+
+import org.asdfgamer.sunriseClock.model.AppDatabase;
+import org.asdfgamer.sunriseClock.model.endpoint.builder.EndpointBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EndpointManager {
+
+    private static EndpointManager endpointManager;
+
+    private final EndpointConfigDao configDAO;
+
+    private Map<Long, BaseEndpoint> endpointCache = new HashMap<>();
+
+    public static EndpointManager getEndpointManager(Context context){
+        if (endpointManager == null){
+            endpointManager = new EndpointManager(context);
+        }
+        return endpointManager;
+    }
+
+    private EndpointManager(Context context){
+        AppDatabase database =  AppDatabase.getInstance(context);
+        this.configDAO = database.endpointConfigDao();
+    }
+
+    public BaseEndpoint getEndpoint(EndpointConfig config){
+        if (!endpointCache.containsKey(config.id)){
+            endpointCache.put(config.id,createEndpoint(config));
+        }
+        return endpointCache.get(config.id);
+    }
+
+    public BaseEndpoint getEndpoint(long id){
+        if (!endpointCache.containsKey(id)){
+            EndpointConfig config = getConfig(id);
+            endpointCache.put(id,createEndpoint(config));
+        }
+        return endpointCache.get(id);
+    }
+
+    private EndpointConfig getConfig(long id){
+        return configDAO.load(id);
+    }
+
+    private BaseEndpoint createEndpoint(EndpointConfig config){
+        EndpointBuilder builder = config.type.getBuilder();
+        return builder.setConfig(config).build();
+    }
+
+}
--- app/src/main/java/org/asdfgamer/sunriseClock/model/endpoint/EndpointType.java
+++ app/src/main/java/org/asdfgamer/sunriseClock/model/endpoint/EndpointType.java
@@ -1,19 +1,29 @@
 package org.asdfgamer.sunriseClock.model.endpoint;
 
+import org.asdfgamer.sunriseClock.model.endpoint.builder.DeconzEndpointBuilder;
+import org.asdfgamer.sunriseClock.model.endpoint.builder.EndpointBuilder;
+
 /**
  * Lists all currently implemented types of (remote) enpoints.
  */
 public enum EndpointType {
 
-    DECONZ(0);
+    DECONZ(0, new DeconzEndpointBuilder());
 
     private int id;
 
-    EndpointType(int id) {
+    private EndpointBuilder builder;
+
+    EndpointType(int id, EndpointBuilder builder) {
         this.id = id;
+        this.builder = builder;
     }
 
     public int getId() {
         return this.id;
+    }
+
+    public EndpointBuilder getBuilder() {
+        return builder;
     }
 }
