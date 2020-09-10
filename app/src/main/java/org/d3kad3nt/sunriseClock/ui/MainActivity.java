c5d6c1dd625b19d13afb666dbfd275d8ff6958c4
Christoph Wildhagen <git@christoph-wildhagen.de>
2020-09-10 21:38:17 +0200

    Added Databinding and set toolbar as actionBar.

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/MainActivity.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/MainActivity.java
@@ -3,14 +3,20 @@ package org.d3kad3nt.sunriseClock.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import org.d3kad3nt.sunriseClock.R;
+import org.d3kad3nt.sunriseClock.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
     }
 }
