6093ca9de78fe031b9bdc0daab84c8c7bfe02441
Christoph <git@christoph-wildhagen.de>
2020-09-10 22:09:55 +0200

    Drawer of navigation works

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/MainActivity.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import org.d3kad3nt.sunriseClock.R;
 import org.d3kad3nt.sunriseClock.databinding.ActivityMainBinding;
@@ -11,12 +15,20 @@ import org.d3kad3nt.sunriseClock.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setContentView(binding.getRoot());
+        navController = Navigation.findNavController(this,R.id.main_activity_nav_fragment);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(binding.drawerLayout).build();
+
         setSupportActionBar(binding.toolbar);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(binding.navigationView, navController);
     }
 }
