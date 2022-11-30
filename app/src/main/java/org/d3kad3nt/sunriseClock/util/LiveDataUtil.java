7d07ad43bb52b0cbd28f433eb191193a1ef85964
Christoph Wildhagen <git@christoph-wildhagen.de>
2022-11-30 21:37:05 +0100

    Add observeOnce helper for LiveData

--- app/src/main/java/org/d3kad3nt/sunriseClock/util/LiveDataUtil.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/util/LiveDataUtil.java
@@ -3,6 +3,7 @@ package org.d3kad3nt.sunriseClock.util;
 import android.util.Log;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 public class LiveDataUtil {
 
@@ -10,6 +11,16 @@ public class LiveDataUtil {
         liveData.observeForever(t -> {
             Log.d("LiveDataUtil", "Log Change");
             Log.d(TAG, t.toString());
+        });
+    }
+
+    public static <T> void observeOnce(LiveData<T> liveData, Observer<T> observer){
+        liveData.observeForever(new Observer<T>() {
+            @Override
+            public void onChanged(T t) {
+                observer.onChanged(t);
+                liveData.removeObserver(this);
+            }
         });
     }
 
