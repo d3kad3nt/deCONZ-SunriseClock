aa3f55b4440e8373ac144e422e5ad4c7d27de3f5
Kai-Uwe Lankenau <github@familie-lankenau.de>
2023-01-01 00:27:34 +0100

    Complete UiLight. This fixes the brightness slider in the UI.

--- app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/UILight.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/UILight.java
@@ -1,49 +1,87 @@
 package org.d3kad3nt.sunriseClock.data.model.light;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import org.jetbrains.annotations.Contract;
 
 public class UILight {
 
+    private static final String TAG = "UiLight";
+
+    private final long lightId;
+    private final long endpointId;
+    // endpointLightId not needed in UI
+
     private final String name;
-    private final long id;
-    private final boolean on;
 
-    public UILight(String name, long id, boolean on) {
+    private final boolean isSwitchable;
+    private final boolean isOn;
+
+    private final boolean isDimmable;
+    private final int brightness;
+
+    private final boolean isTemperaturable;
+    //private final int colorTemperature; // Not yet implemented in the backend
+
+    private final boolean isColorable;
+    //private final int color; // Not yet implemented in the backend
+
+    private UILight(long lightId, long endpointId, String name, boolean isSwitchable, boolean isOn, boolean isDimmable, int brightness, boolean isTemperaturable/*, int colorTemperature*/, boolean isColorable/*, int color*/) {
+        this.lightId = lightId;
+        this.endpointId = endpointId;
         this.name = name;
-        this.id = id;
-        this.on = on;
-    }
-
-    public int getColor() {
-        return 0;
-    }
-
-    public int getBrightness() {
-        return 0;
-    }
-
-    public boolean isOn() {
-        return on;
-    }
-
-    public int getColorTemperature() {
-        return 0;
+        this.isSwitchable = isSwitchable;
+        this.isOn = isOn;
+        this.isDimmable = isDimmable;
+        this.brightness = brightness;
+        this.isTemperaturable = isTemperaturable;
+        //this.colorTemperature = colorTemperature;
+        this.isColorable = isColorable;
+        //this.color = color;
     }
 
     public long getLightId() {
-        return id;
+        return lightId;
     }
 
-    public String getFriendlyName() {
+    public long getEndpointId() {
+        return endpointId;
+    }
+
+    public String getName() {
         return name;
+    }
+
+    public boolean getIsSwitchable() {
+        return isSwitchable;
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public boolean getIsDimmable() {
+        return isDimmable;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public boolean getIsTemperaturable() {
+        return isTemperaturable;
+    }
+
+    public boolean getIsColorable() {
+        return isColorable;
     }
 
     @NonNull
     @Contract("_ -> new")
     public static UILight from(@NonNull DbLight dbLight) {
-        //Todo: Logic to convert db light to UI light
-        return new UILight(dbLight.getName(), dbLight.getLightId(), dbLight.getIsOn());
+        // Place for conversion logic (if UI needs other data types or value ranges).
+        return new UILight(dbLight.getLightId(), dbLight.getEndpointId(), dbLight.getName(), dbLight.getIsSwitchable(), dbLight.getIsOn(), dbLight.getIsDimmable(), dbLight.getBrightness(), dbLight.getIsTemperaturable(), dbLight.getIsColorable());
     }
 }
