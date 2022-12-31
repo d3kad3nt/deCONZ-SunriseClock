416cf5f8a384821477b18ff9e645ab810caf73c2
ku-we <github@familie-lankenau.de>
2022-12-31 13:56:24 +0100

    Set explicit @Ignore on data that should not be processed by Room.

--- app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/DbLight.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/DbLight.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
@@ -22,6 +23,7 @@ import org.jetbrains.annotations.Contract;
                 onDelete = ForeignKey.CASCADE))
 public class DbLight {
 
+    @Ignore
     public static final String TABLENAME = "light";
 
     @PrimaryKey(autoGenerate = true)
