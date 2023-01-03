ab31409e6abcff8fd135ec2b91301ada073cce3c
Kai-Uwe Lankenau <github@familie-lankenau.de>
2023-01-03 22:43:46 +0100

    Remove todo from RemoteLightBuilder.
    Presence of IDs is checked inside RemoteLight constructor.

--- app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/RemoteLightBuilder.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/data/model/light/RemoteLightBuilder.java
@@ -33,8 +33,6 @@ public class RemoteLightBuilder {
     }
 
     public RemoteLight build() {
-        //Todo: Check if endpointId and endpointLightId are set, problem: RemoteLightListTypeAdapter has to set
-        // these values after the single light was parsed by GSON
         if (endpointType == null) {
             throw new IllegalStateException(
                 "RemoteLightBuilder cannot build this light without an endpoint type! " + "Check remote light" +
