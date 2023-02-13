7ffc4fa576533ce89946a1a84132729ea61fd6f6
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-02-13 17:35:13 +0100

    Provide real lights in createPublisherForAllAvailable
    This code has to be refactored before it is merged into master

--- app/src/main/java/org/d3kad3nt/sunriseClock/deviceControl/ControlService.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/deviceControl/ControlService.java
@@ -11,9 +11,17 @@ import android.service.controls.actions.ControlAction;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.lifecycle.LiveData;
 
-import org.d3kad3nt.sunriseClock.R;
-import org.d3kad3nt.sunriseClock.util.FlowUtil;
+import org.d3kad3nt.sunriseClock.data.model.endpoint.IEndpointUI;
+import org.d3kad3nt.sunriseClock.data.model.light.UILight;
+import org.d3kad3nt.sunriseClock.data.model.resource.Resource;
+import org.d3kad3nt.sunriseClock.data.model.resource.Status;
+import org.d3kad3nt.sunriseClock.data.repository.EndpointRepository;
+import org.d3kad3nt.sunriseClock.data.repository.LightRepository;
+import org.d3kad3nt.sunriseClock.util.AsyncJoinHelper;
+import org.d3kad3nt.sunriseClock.util.ExtendedPublisher;
+import org.d3kad3nt.sunriseClock.util.LiveDataUtil;
 
 import java.util.List;
 import java.util.concurrent.Flow;
@@ -22,19 +30,58 @@ import java.util.function.Consumer;
 @RequiresApi(api = Build.VERSION_CODES.R)
 public class ControlService extends ControlsProviderService {
 
+    private static final String TAG = "ControlService";
+
     @NonNull
     @Override
     public Flow.Publisher<Control> createPublisherForAllAvailable() {
-        Context context = getBaseContext();
+        final Context context = getBaseContext();
+        final EndpointRepository endpointRepository = EndpointRepository.getInstance(context);
+        final LightRepository lightRepository = LightRepository.getInstance(context);
         Intent intent = new Intent();
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, intent,
             PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        String app_name = context.getResources().getString(R.string.app_name);
-        Control control =
-            new Control.StatelessBuilder("sunrise-test1", pendingIntent).setTitle("Test 1").setSubtitle("Subtitle")
-                .setStructure(app_name).setDeviceType(DeviceTypes.TYPE_LIGHT).build();
-        List<Control> list = List.of(control);
-        return FlowUtil.publishList(list);
+        ExtendedPublisher<Control> flow = new ExtendedPublisher<>(true);
+        LiveData<List<IEndpointUI>> allEndpoints = endpointRepository.getAllEndpoints();
+        AsyncJoinHelper asyncHelper = new AsyncJoinHelper();
+
+        LiveDataUtil.observeOnce(allEndpoints, new AsyncJoinHelper.Observer<>(asyncHelper) {
+            @Override
+            public void onChanged(final List<IEndpointUI> endpoints) {
+                for (IEndpointUI endpoint : endpoints) {
+                    LiveData<Resource<List<UILight>>> lightResources =
+                        lightRepository.getLightsForEndpoint(endpoint.getId());
+                    lightResources.observeForever(new AsyncJoinHelper.Observer<>(asyncHelper) {
+                        @Override
+                        public void onChanged(final Resource<List<UILight>> listResource) {
+                            if (listResource.getStatus() == Status.LOADING) {
+                                return;
+                            }
+                            if (listResource.getStatus() == Status.ERROR) {
+                                lightResources.removeObserver(this);
+                                asyncHelper.removeAsyncTask(this);
+                                return;
+                            }
+                            List<UILight> lights = listResource.getData();
+                            for (UILight light : lights) {
+                                Control.StatelessBuilder builder =
+                                    new Control.StatelessBuilder(getControlId(light), pendingIntent);
+                                builder.setTitle(light.getName());
+                                //TODO use endpoint.getName, when it is merged
+                                builder.setStructure(endpoint.getStringRepresentation());
+                                builder.setDeviceType(DeviceTypes.TYPE_LIGHT);
+                                flow.publish(builder.build());
+                            }
+                            lightResources.removeObserver(this);
+                            asyncHelper.removeAsyncTask(this);
+                        }
+                    });
+                }
+                asyncHelper.removeAsyncTask(this);
+            }
+        });
+        asyncHelper.executeWhenJoined(() -> flow.complete());
+        return flow;
     }
 
     @NonNull
@@ -55,5 +102,10 @@ public class ControlService extends ControlsProviderService {
     public void performControlAction(@NonNull final String controlId, @NonNull final ControlAction action,
                                      @NonNull final Consumer<Integer> consumer) {
 
+    }
+
+    private LiveData<Resource<UILight>> getUILight(String controlId, LightRepository lightRepository) {
+        long lightID = Long.parseLong(controlId.split("-")[2]);
+        return lightRepository.getLight(lightID);
     }
 }
