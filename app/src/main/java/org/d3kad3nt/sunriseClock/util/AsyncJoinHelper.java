62e47d39508156f084bd1d0efca32933466de4e6
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-02-13 17:33:35 +0100

    Added AsyncJoinHelper which runs tasks after async tasks are finished
    This can be used in callbacks (for example liveData observers)

---
+++ app/src/main/java/org/d3kad3nt/sunriseClock/util/AsyncJoinHelper.java
@@ -0,0 +1,51 @@
+package org.d3kad3nt.sunriseClock.util;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AsyncJoinHelper {
+
+    private final static String TAG = "AsyncJoinHelper";
+    private final MutableLiveData<Boolean> joinState = new MutableLiveData<>();
+    private final Set<Object> observers = new HashSet<>();
+    private boolean somethingObserved = false;
+
+    public void addAsyncTask(Object task) {
+        somethingObserved = true;
+        observers.add(task);
+    }
+
+    public void removeAsyncTask(Object task) {
+        observers.remove(task);
+        if (observers.isEmpty()) {
+            joinState.setValue(somethingObserved);
+        }
+    }
+
+    public void executeWhenJoined(Action action) {
+        joinState.observeForever(new androidx.lifecycle.Observer<>() {
+            @Override
+            public void onChanged(final Boolean completed) {
+                if (completed) {
+                    action.apply();
+                    joinState.removeObserver(this);
+                }
+            }
+        });
+    }
+
+    @FunctionalInterface
+    public interface Action {
+
+        void apply();
+    }
+
+    public static abstract class Observer <T> implements androidx.lifecycle.Observer<T> {
+
+        public Observer(AsyncJoinHelper joinHelper) {
+            joinHelper.addAsyncTask(this);
+        }
+    }
+}
