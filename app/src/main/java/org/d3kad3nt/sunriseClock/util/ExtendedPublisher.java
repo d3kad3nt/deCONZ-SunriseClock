b099312507ab02ef36d89b235903b3a9198de98d
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-02-23 22:16:24 +0100

    Fixed ExtendedPublisher implementation
    The previous implementation wasn't correct and didn't work correctly.
    This should work better now

--- app/src/main/java/org/d3kad3nt/sunriseClock/util/ExtendedPublisher.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/util/ExtendedPublisher.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Flow;
+import java.util.function.Consumer;
 
 @RequiresApi(api = Build.VERSION_CODES.R)
 public class ExtendedPublisher <T> implements Flow.Publisher<T> {
 
     private final List<Flow.Subscriber<? super T>> subscriberList = new LinkedList<>();
+    private final List<ExtendedSubscription> subscriptionList = new LinkedList<>();
 
     private final boolean cache_publish_values;
 
@@ -33,7 +35,9 @@ public class ExtendedPublisher <T> implements Flow.Publisher<T> {
 
     @Override
     public void subscribe(final Flow.Subscriber<? super T> subscriber) {
-        subscriberList.add(subscriber);
+        ExtendedSubscription subscription = new ExtendedSubscription(subscriber);
+        subscriber.onSubscribe(subscription);
+        subscriptionList.add(subscription);
         if (cache_publish_values) {
             for (T value : cache) {
                 subscriber.onNext(value);
@@ -42,14 +46,48 @@ public class ExtendedPublisher <T> implements Flow.Publisher<T> {
     }
 
     public void publish(@NonNull T value) {
-        subscriberList.forEach(subscriber -> subscriber.onNext(value));
-        if (cache_publish_values) {
-            cache.add(value);
-        }
+        subscriptionList.forEach(new Consumer<>() {
+            @Override
+            public void accept(final ExtendedSubscription subscription) {
+                if (subscription.requestedItems > 0) {
+                    subscription.subscriber.onNext(value);
+                    subscription.requestedItems--;
+                    subscription.currentListPos++;
+                }
+            }
+        });
+        cache.add(value);
     }
 
     public void complete() {
         subscriberList.forEach(subscriber -> subscriber.onComplete());
         cache.clear();
+    }
+
+    class ExtendedSubscription implements Flow.Subscription {
+
+        private final Flow.Subscriber<? super T> subscriber;
+        private int currentListPos = cache_publish_values ? 0 : cache.size();
+
+        private long requestedItems = 0;
+
+        ExtendedSubscription(Flow.Subscriber<? super T> subscriber) {
+            this.subscriber = subscriber;
+        }
+
+        @Override
+        public void request(final long n) {
+            requestedItems = n;
+            while (currentListPos < cache.size() && requestedItems > 0) {
+                subscriber.onNext(cache.get(currentListPos));
+                currentListPos++;
+                requestedItems--;
+            }
+        }
+
+        @Override
+        public void cancel() {
+            subscriberList.remove(subscriber);
+        }
     }
 }
