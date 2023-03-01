3274e60d9ce20fcc3f25e029c209154a713c36b3
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-03-01 22:59:26 +0100

    Added AppBar

--- app/src/main/java/org/d3kad3nt/sunriseClock/deviceControl/ControlActivity.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/deviceControl/ControlActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import org.d3kad3nt.sunriseClock.R;
+import org.d3kad3nt.sunriseClock.databinding.ActivityDeviceControlBinding;
 
 public class ControlActivity extends AppCompatActivity {
 
@@ -21,9 +25,8 @@ public class ControlActivity extends AppCompatActivity {
         args.putLong("Light", getIntent().getLongExtra("Light", -1));
         args.putString("LightName", getIntent().getStringExtra("LightName"));
         Log.d(TAG, args.toString());
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_device_control);
-        setContentView(R.layout.activity_device_control);
-//        setContentView(binding.getRoot());
+        ActivityDeviceControlBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_device_control);
+        setContentView(binding.getRoot());
         NavHostFragment navHostFragment =
             (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.device_control_activity_nav_fragment);
         if (navHostFragment == null) {
@@ -32,11 +35,9 @@ public class ControlActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
 
         navController.setGraph(R.navigation.nav_graph_device_control, args);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
 
-//        navController.navigate(R.id.lightDetail, args);
-//        setSupportActionBar(binding.toolbar);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navigationView, navController);
+        setSupportActionBar(binding.toolbar);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 }
