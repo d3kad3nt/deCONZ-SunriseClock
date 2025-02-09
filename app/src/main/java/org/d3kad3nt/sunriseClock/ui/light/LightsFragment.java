ca80e6fb7506ab71cb75922929a41dec7a280c41
Christoph Wildhagen <git@christoph-wildhagen.de>
2025-02-09 20:51:11 +0100

    Remove Endpoint spinner

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightsFragment.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightsFragment.java
@@ -5,24 +5,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Spinner;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import org.d3kad3nt.sunriseClock.R;
-import org.d3kad3nt.sunriseClock.data.model.endpoint.IEndpointUI;
 import org.d3kad3nt.sunriseClock.data.model.light.UILight;
 import org.d3kad3nt.sunriseClock.data.model.resource.Resource;
 import org.d3kad3nt.sunriseClock.data.model.resource.Status;
 import org.d3kad3nt.sunriseClock.databinding.LightsFragmentBinding;
-import org.d3kad3nt.sunriseClock.ui.MainActivity;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -34,7 +30,6 @@ public class LightsFragment extends Fragment implements LightsListAdapter.ClickL
     private final LightsState lightsState = new LightsState();
     private LightsFragmentBinding binding;
     private LightsViewModel viewModel;
-    private Spinner endpointSpinner;
     private LightsListAdapter adapter;
 
     @Override
@@ -69,8 +64,6 @@ public class LightsFragment extends Fragment implements LightsListAdapter.ClickL
             }
         });
 
-        addEndpointSelector();
-
         return binding.getRoot();
     }
 
@@ -85,55 +78,7 @@ public class LightsFragment extends Fragment implements LightsListAdapter.ClickL
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        removeFromToolbar(endpointSpinner);
         binding = null;
-    }
-
-    private void addEndpointSelector() {
-        endpointSpinner = new Spinner(getContext());
-        EndpointSelectorAdapter adapter =
-            new EndpointSelectorAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item);
-        endpointSpinner.setAdapter(adapter);
-        endpointSpinner.setOnItemSelectedListener(new EndpointSelectedListener(getContext()));
-
-        viewModel.getEndpoints().observe(getViewLifecycleOwner(), configList -> {
-            adapter.submitCollection(configList);
-            if (!configList.isEmpty()) {
-                addToToolbar(endpointSpinner);
-                lightsState.clearError();
-            } else {
-                removeFromToolbar(endpointSpinner);
-                lightsState.setError(getResources().getString(R.string.noEndpoint_title),
-                    getResources().getString(R.string.noEndpoint_message));
-            }
-        });
-
-        viewModel.getSelectedEndpoint().observe(getViewLifecycleOwner(), endpointConfig -> {
-
-            Observer<? super List<IEndpointUI>> endpointSelector = new Observer<List<IEndpointUI>>() {
-                @Override
-                public void onChanged(List<IEndpointUI> endpointConfigs) {
-                    endpointSpinner.setSelection(endpointConfigs.indexOf(endpointConfig));
-                }
-            };
-            viewModel.getEndpoints().observe(getViewLifecycleOwner(), endpointSelector);
-            viewModel.getEndpoints().removeObserver(endpointSelector);
-        });
-    }
-
-    private void removeFromToolbar(View view) {
-        if (getActivity() != null && getActivity() instanceof MainActivity) {
-            ((MainActivity) getActivity()).getToolbar().removeView(view);
-        }
-    }
-
-    private void addToToolbar(View view) {
-        if (getActivity() != null && getActivity() instanceof MainActivity) {
-            Toolbar toolbar = ((MainActivity) getActivity()).getToolbar();
-            if (toolbar.indexOfChild(view) == -1) {
-                toolbar.addView(view);
-            }
-        }
     }
 
     @Override
