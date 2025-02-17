edf77f447e9816a7e782c36c100897738f25383d
Christoph Wildhagen <git@christoph-wildhagen.de>
2025-02-17 20:22:09 +0100

    Improve endpoint selection (#86)
    Move Endpoint Selection from Light List to Endpoint List
    
    ---------
    
    Co-authored-by: Kai-Uwe Lankenau <github@familie-lankenau.de>

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/EndpointsListAdapter.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/EndpointsListAdapter.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
 import androidx.navigation.Navigation;
@@ -16,8 +18,17 @@ import org.d3kad3nt.sunriseClock.databinding.EndpointListElementBinding;
 
 public class EndpointsListAdapter extends ListAdapter<IEndpointUI, EndpointsListAdapter.ViewHolder> {
 
-    public EndpointsListAdapter() {
+    private static final String TAG = EndpointsListAdapter.class.getSimpleName();
+    private final EndpointsViewModel viewModel;
+
+    private CompoundButton selectedRadioButton = null;
+
+    private final ClickListeners clickListeners;
+
+    public EndpointsListAdapter(final EndpointsViewModel viewModel, final ClickListeners clickListeners) {
         super(new EndpointDiffCallback());
+        this.viewModel = viewModel;
+        this.clickListeners = clickListeners;
     }
 
     @NonNull
@@ -30,28 +41,62 @@ public class EndpointsListAdapter extends ListAdapter<IEndpointUI, EndpointsList
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         IEndpointUI endpoint = getItem(position);
-        holder.bind(createOnClickListener(endpoint.getId()), endpoint);
+        holder.bind(endpoint);
         holder.itemView.setTag(endpoint);
     }
 
-    private View.OnClickListener createOnClickListener(long endpointID) {
-        return v -> Navigation.findNavController(v)
-            .navigate(EndpointsFragmentDirections.actionEndpointsToEndpointDetail(endpointID));
+    public interface ClickListeners {
+
+        /**
+         * Navigates to the endpoint detail screen, providing detailed information for this endpoint.
+         *
+         * @param view         View representing the endpoint card.
+         * @param endpointId   The unique identifier for this endpoint.
+         */
+        void onCardClick(View view, long endpointId);
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         private final EndpointListElementBinding binding;
+        private IEndpointUI endpoint;
 
         ViewHolder(@NonNull EndpointListElementBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
 
-        void bind(View.OnClickListener listener, IEndpointUI item) {
-            binding.setClickListener(listener);
+        void bind(IEndpointUI item) {
+            binding.setCardClickListener(new CardClickListener());
             binding.setEndpoint(item);
+            binding.setRadioCheckedChangeListener(new RadioCheckedChangeListener());
             binding.executePendingBindings();
+            this.endpoint = item;
+            binding.setEndpointSelected(viewModel.isSelectedEndpoint(item.getId()));
+        }
+
+        public class CardClickListener implements View.OnClickListener {
+
+            @Override
+            public void onClick(final View view) {
+                IEndpointUI endpoint = getItem(getAbsoluteAdapterPosition());
+                clickListeners.onCardClick(view, endpoint.getId());
+            }
+        }
+
+        public class RadioCheckedChangeListener implements RadioButton.OnCheckedChangeListener {
+
+            @Override
+            public void onCheckedChanged(final CompoundButton compoundButton, final boolean checkedState) {
+                if (!checkedState){
+                    return;
+                }
+                if (selectedRadioButton != null){
+                    selectedRadioButton.setChecked(false);
+                }
+                selectedRadioButton = compoundButton;
+                viewModel.setSelectedEndpoint(endpoint.getId());
+            }
         }
     }
 
--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/EndpointsViewModel.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/EndpointsViewModel.java
@@ -8,22 +8,37 @@ import androidx.lifecycle.LiveData;
 
 import org.d3kad3nt.sunriseClock.data.model.endpoint.IEndpointUI;
 import org.d3kad3nt.sunriseClock.data.repository.EndpointRepository;
+import org.d3kad3nt.sunriseClock.data.repository.SettingsRepository;
 
 import java.util.List;
 
 public class EndpointsViewModel extends AndroidViewModel {
 
-    private final EndpointRepository endpointRepository =
-        EndpointRepository.getInstance(getApplication().getApplicationContext());
     private final LiveData<List<IEndpointUI>> endpoints;
+    private final SettingsRepository settingsRepository;
 
     public EndpointsViewModel(@NonNull Application application) {
         super(application);
-        //TODO use something better
+        EndpointRepository endpointRepository =
+            EndpointRepository.getInstance(getApplication().getApplicationContext());
         endpoints = endpointRepository.getAllEndpoints();
+        settingsRepository = SettingsRepository.getInstance(application.getApplicationContext());
     }
 
     public LiveData<List<IEndpointUI>> getEndpoints() {
         return endpoints;
+    }
+
+    public void setSelectedEndpoint(final long id) {
+        settingsRepository.setActiveEndpoint(id);
+        System.out.println(settingsRepository.getActiveEndpoint());
+    }
+
+    public boolean isSelectedEndpoint(final long id) {
+        try {
+            return settingsRepository.getActiveEndpoint() == id;
+        } catch (IllegalStateException e){
+            return false;
+        }
     }
 }
