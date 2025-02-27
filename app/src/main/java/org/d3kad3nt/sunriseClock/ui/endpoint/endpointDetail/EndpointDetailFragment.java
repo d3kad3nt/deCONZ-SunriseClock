2620c42b68dc31a9201ffad237683378a3db6698
Kai-Uwe Lankenau <github@familie-lankenau.de>
2025-02-28 00:58:04 +0100

    Implement new way to get dependencies into viewModels.
    Since version 2.5.0 extending ViewModelProvider.Factory can be replaced by a static ViewModelInitializer.

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/endpointDetail/EndpointDetailFragment.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/endpointDetail/EndpointDetailFragment.java
@@ -9,7 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.viewmodel.MutableCreationExtras;
 
+import org.d3kad3nt.sunriseClock.data.repository.EndpointRepository;
+import org.d3kad3nt.sunriseClock.data.repository.SettingsRepository;
 import org.d3kad3nt.sunriseClock.databinding.EndpointDetailFragmentBinding;
 import org.d3kad3nt.sunriseClock.util.LogUtil;
 
@@ -22,13 +25,20 @@ public class EndpointDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         LogUtil.d("Show endpoint detail view");
-        long endpointID =
-            EndpointDetailFragmentArgs.fromBundle(requireArguments()).getEndpointID(); // id from navigation
-        // Use custom factory to initialize viewModel with endpoint id (instead of using new ViewModelProvider
-        // (this).get(EndpointDetailViewModel.class))
-        viewModel = new ViewModelProvider(this,
-            new EndpointDetailViewModelFactory(requireActivity().getApplication(), endpointID)).get(
-            EndpointDetailViewModel.class);
+        long endpointID = EndpointDetailFragmentArgs.fromBundle(requireArguments()).getEndpointID(); // id from navigation
+
+        // Initialize viewModel with endpoint id and inject the endpoint and settings repository.
+        // By injecting the repository, the viewModel no longer needs the Application or Context.
+        MutableCreationExtras viewModelDependencies = new MutableCreationExtras();
+        viewModelDependencies.set(EndpointDetailViewModel.ENDPOINT_REPOSITORY_KEY, EndpointRepository.getInstance(requireContext()));
+        viewModelDependencies.set(EndpointDetailViewModel.SETTINGS_REPOSITORY_KEY, SettingsRepository.getInstance(requireContext()));
+        viewModelDependencies.set(EndpointDetailViewModel.ENDPOINT_ID_KEY, endpointID);
+
+        // Use custom factory to initialize the viewModel (instead of using new ViewModelProvider(this).get(EndpointDetailViewModel.class)).
+        // For viewModel older than 2.5.0 ViewModelProvider.Factory had to be extended.
+        viewModel = new ViewModelProvider(this.getViewModelStore(),
+            ViewModelProvider.Factory.from(EndpointDetailViewModel.initializer), viewModelDependencies).get(EndpointDetailViewModel.class);
+
         binding = EndpointDetailFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/endpointDetail/EndpointDetailViewModel.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/endpoint/endpointDetail/EndpointDetailViewModel.java
@@ -1,11 +1,11 @@
 package org.d3kad3nt.sunriseClock.ui.endpoint.endpointDetail;
 
-import android.app.Application;
-
 import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.viewmodel.CreationExtras;
+import androidx.lifecycle.viewmodel.ViewModelInitializer;
 
 import org.d3kad3nt.sunriseClock.data.model.endpoint.IEndpointUI;
 import org.d3kad3nt.sunriseClock.data.repository.EndpointRepository;
@@ -15,21 +15,29 @@ import java.util.Optional;
 
 import kotlin.jvm.functions.Function1;
 
-public class EndpointDetailViewModel extends AndroidViewModel {
+public class EndpointDetailViewModel extends ViewModel {
 
-    private final EndpointRepository endpointRepository =
-        EndpointRepository.getInstance(getApplication().getApplicationContext());
+    public static final CreationExtras.Key<EndpointRepository> ENDPOINT_REPOSITORY_KEY = new CreationExtras.Key<>() {};
+    private final EndpointRepository endpointRepository;
 
-    private final SettingsRepository settingsRepository =
-        SettingsRepository.getInstance(getApplication().getApplicationContext());
+    public final static CreationExtras.Key<SettingsRepository> SETTINGS_REPOSITORY_KEY = new CreationExtras.Key<>() {};
+    private final SettingsRepository settingsRepository;
+
+    public final static CreationExtras.Key<Long> ENDPOINT_ID_KEY = new CreationExtras.Key<>() {};
+    private final long endpointID;
 
     public LiveData<IEndpointUI> endpointConfig;
 
     public LiveData<Boolean> selected;
 
-    public EndpointDetailViewModel(@NonNull Application application, long endpointId) {
-        super(application);
-        endpointConfig = getEndpoint(endpointId);
+    public EndpointDetailViewModel(@NonNull EndpointRepository endpointRepository, @NonNull SettingsRepository settingsRepository, long endpointId) {
+        super();
+        this.endpointRepository = endpointRepository;
+        this.settingsRepository = settingsRepository;
+        this.endpointID = endpointId;
+
+        this.endpointConfig = getEndpoint(endpointId);
+
         selected = Transformations.map(settingsRepository.getActiveEndpointIdAsLivedata(), new Function1<>() {
             @Override
             public Boolean invoke(final Optional<Long> aLong) {
@@ -41,4 +49,14 @@ public class EndpointDetailViewModel extends AndroidViewModel {
     private LiveData<IEndpointUI> getEndpoint(long endpointID) {
         return endpointRepository.getEndpoint(endpointID);
     }
+
+    static final ViewModelInitializer<EndpointDetailViewModel> initializer = new ViewModelInitializer<>(
+        EndpointDetailViewModel.class,
+        creationExtras -> {
+            EndpointRepository endpointRepository = creationExtras.get(ENDPOINT_REPOSITORY_KEY);
+            SettingsRepository settingsRepository = creationExtras.get(SETTINGS_REPOSITORY_KEY);
+            Long endpointId = creationExtras.get(ENDPOINT_ID_KEY);
+            return new EndpointDetailViewModel(endpointRepository, settingsRepository, endpointId);
+        }
+    );
 }
