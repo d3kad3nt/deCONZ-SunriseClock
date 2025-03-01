c9f503dc41e8af2b0d4e005fe7a205dee55c88db
Christoph Wildhagen <git@christoph-wildhagen.de>
2025-03-01 01:18:03 +0100

    Add setPrefix Method
    This can be used to add an Prefix for Log Messages in the current class

--- app/src/main/java/org/d3kad3nt/sunriseClock/util/LogUtil.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/util/LogUtil.java
@@ -2,9 +2,20 @@ package org.d3kad3nt.sunriseClock.util;
 
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LogUtil {
+
+    private static final Map<String, String> prefixCache = new HashMap<>();
+
     public static void v(String message){
-        Log.v(getCallerClassName(), message);
+        String callerClass = getCallerClassName();
+        if (prefixCache.containsKey(callerClass)) {
+            Log.v(getCallerClassSimpleName(), prefixCache.get(callerClass) + message);
+        } else {
+            Log.v(getCallerClassSimpleName(), message);
+        }
     }
 
     public static void v(String message, Object ... parameters){
@@ -12,7 +23,12 @@ public class LogUtil {
     }
 
     public static void d(String message){
-        Log.d(getCallerClassName(), message);
+        String callerClass = getCallerClassName();
+        if (prefixCache.containsKey(callerClass)) {
+            Log.d(getCallerClassSimpleName(), prefixCache.get(callerClass) + message);
+        } else {
+            Log.d(getCallerClassSimpleName(), message);
+        }
     }
 
     public static void d(String message, Object ... parameters){
@@ -20,7 +36,12 @@ public class LogUtil {
     }
 
     public static void i(String message){
-        Log.i(getCallerClassName(), message);
+        String callerClass = getCallerClassName();
+        if (prefixCache.containsKey(callerClass)) {
+            Log.i(getCallerClassSimpleName(), prefixCache.get(callerClass) + message);
+        } else {
+            Log.i(getCallerClassSimpleName(), message);
+        }
     }
 
     public static void i(String message, Object... parameters){
@@ -28,7 +49,12 @@ public class LogUtil {
     }
 
     public static void w(String message){
-        Log.w(getCallerClassName(), message);
+        String callerClass = getCallerClassName();
+        if (prefixCache.containsKey(callerClass)) {
+            Log.w(getCallerClassSimpleName(), prefixCache.get(callerClass) + message);
+        } else {
+            Log.w(getCallerClassSimpleName(), message);
+        }
     }
 
     public static void w(String message, Object... parameters){
@@ -36,22 +62,38 @@ public class LogUtil {
     }
 
     public static void e(String message){
-        Log.e(getCallerClassName(), message);
+        String callerClass = getCallerClassName();
+        if (prefixCache.containsKey(callerClass)) {
+            Log.e(getCallerClassSimpleName(), prefixCache.get(callerClass) + message);
+        } else {
+            Log.e(getCallerClassSimpleName(), message);
+        }
     }
 
     public static void e(String message, Object... parameters){
         e(String.format(message, parameters));
     }
 
+    private static String getCallerClassSimpleName() {
+        String[] splitClassName = getCallerClassName().split("\\.");
+        return splitClassName[splitClassName.length - 1];
+    }
     private static String getCallerClassName() {
         StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
         for (int i=1; i<stElements.length; i++) {
             StackTraceElement ste = stElements[i];
             if (!ste.getClassName().equals(LogUtil.class.getName()) && ste.getClassName().indexOf("java.lang.Thread")!=0) {
-                String[] splitClassName = ste.getClassName().split("\\.");
-                return splitClassName[splitClassName.length-1];
+                return ste.getClassName();
             }
         }
         return "No Class Found";
+    }
+
+    public static void setPrefix(final String prefix) {
+        prefixCache.put(getCallerClassName(), prefix);
+    }
+
+    public static void setPrefix(final String prefix, Object... parameters) {
+        setPrefix(String.format(prefix, parameters));
     }
 }
