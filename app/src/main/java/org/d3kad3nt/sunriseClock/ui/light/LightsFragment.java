e4ced7a0feb26bbb4c5a17bed044f7124a3b746c
ku-we <github@familie-lankenau.de>
2025-03-30 19:24:42 +0200

    Add base fragment. Use base fragment in lights fragment.

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightsFragment.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightsFragment.java
@@ -1,27 +1,18 @@
 package org.d3kad3nt.sunriseClock.ui.light;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.view.MenuProvider;
-import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 import org.d3kad3nt.sunriseClock.R;
 import org.d3kad3nt.sunriseClock.data.model.ListItem;
 import org.d3kad3nt.sunriseClock.data.model.group.UIGroup;
@@ -29,28 +20,39 @@ import org.d3kad3nt.sunriseClock.data.model.light.UILight;
 import org.d3kad3nt.sunriseClock.data.model.resource.Resource;
 import org.d3kad3nt.sunriseClock.data.model.resource.Status;
 import org.d3kad3nt.sunriseClock.databinding.LightsFragmentBinding;
+import org.d3kad3nt.sunriseClock.ui.util.BaseFragment;
 import org.d3kad3nt.sunriseClock.util.LogUtil;
 
-public class LightsFragment extends Fragment implements LightsListAdapter.ClickListeners, MenuProvider {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LightsFragment extends BaseFragment<LightsFragmentBinding, LightsViewModel> implements LightsListAdapter.ClickListeners, MenuProvider {
 
     private final LightsState lightsState = new LightsState();
-    private LightsFragmentBinding binding;
-    private LightsViewModel viewModel;
     private LightsListAdapter adapter;
 
+        // Initialize the options menu (toolbar menu).
+        //binding.lightsToolbar.addMenuProvider(this, getViewLifecycleOwner());
+
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LogUtil.d("Show light list view");
-        viewModel = new ViewModelProvider(requireActivity()).get(LightsViewModel.class);
+    protected LightsFragmentBinding getViewBinding() {
+        return LightsFragmentBinding.inflate(getLayoutInflater());
+    }
 
-        binding = LightsFragmentBinding.inflate(inflater, container, false);
+    @Override
+    protected Class<LightsViewModel> getViewModelClass() {
+        return LightsViewModel.class;
+    }
 
+    @Override
+    protected void bindVars(final LightsFragmentBinding binding) {
+        binding.setViewModel(viewModel);
         binding.setLightsState(lightsState);
 
         adapter = new LightsListAdapter(this);
         binding.recyclerView.setAdapter(adapter);
-
         viewModel.getGroupsWithLights().observe(getViewLifecycleOwner(), new Observer<>() {
             @Override
             public void onChanged(final Resource<Map<UIGroup, List<UILight>>> mapResource) {
@@ -60,11 +62,11 @@ public class LightsFragment extends Fragment implements LightsListAdapter.ClickL
 
                     // Use comparable interface in UIGroup (and then UIList) to sort the output.
                     List<ListItem> flatList = mapResource.getData().entrySet().stream()
-                            .sorted(Map.Entry.comparingByKey())
-                            .flatMap(entry -> Stream.concat(
-                                    Stream.of(entry.getKey()),
-                                    entry.getValue().stream().sorted()))
-                            .collect(Collectors.toUnmodifiableList());
+                        .sorted(Map.Entry.comparingByKey())
+                        .flatMap(entry -> Stream.concat(
+                            Stream.of(entry.getKey()),
+                            entry.getValue().stream().sorted()))
+                        .collect(Collectors.toUnmodifiableList());
 
                     adapter.submitList(flatList);
                 } else if (mapResource.getStatus().equals(Status.ERROR)) {
@@ -74,40 +76,16 @@ public class LightsFragment extends Fragment implements LightsListAdapter.ClickL
                 }
             }
         });
-
-        // Initialize the options menu (toolbar menu).
-        binding.lightsToolbar.addMenuProvider(this, getViewLifecycleOwner());
-
-        return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        NavController navController = Navigation.findNavController(view);
-
-        // In some cases, you might need to define multiple top-level destinations instead of using
-        // the default start
-        // destination.
-        // Using a BottomNavigationView is a common use case for this, where you may have sibling
-        // screens that are
-        // not hierarchically related to each other and may each have their own set of related
-        // destinations.
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(R.id.lightsList, R.id.endpointsList, R.id.mainSettingsFragment).build();
-
-        NavigationUI.setupWithNavController(binding.lightsToolbar, navController, appBarConfiguration);
-
-        binding.setViewModel(viewModel);
-        // Specify the fragment view as the lifecycle owner of the binding. This is used so that the
-        // binding can
-        // observe LiveData updates.
-        binding.setLifecycleOwner(getViewLifecycleOwner());
+    protected LifecycleOwner getLifecycleOwner() {
+        return getViewLifecycleOwner();
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    protected ViewModelProvider getViewModelProvider() {
+        return new ViewModelProvider(requireActivity());
     }
 
     @Override
