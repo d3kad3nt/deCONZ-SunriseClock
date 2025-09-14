6173d1f7b5a0aa0de3b213dbdae959c14528eee5
ku-we <github@familie-lankenau.de>
2025-09-14 10:44:44 +0200

    Comments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.13.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -20,8 +20,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-
-//tasks.register("clean", Delete) {
-//    delete(rootProject.layout.buildDirectory)
-//}
