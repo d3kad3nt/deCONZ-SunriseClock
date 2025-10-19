e1c47c5fad895825e62716d7fdb9903e37841059
Kai-Uwe Lankenau <github@familie-lankenau.de>
2025-10-19 22:12:52 +0200

    Make LightDetail and GroupDetail more similiar.

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/group/groupDetail/GroupDetailFragment.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/group/groupDetail/GroupDetailFragment.java
@@ -9,27 +9,19 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.viewmodel.MutableCreationExtras;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import org.d3kad3nt.sunriseClock.R;
 import org.d3kad3nt.sunriseClock.backend.data.repository.LightRepository;
 import org.d3kad3nt.sunriseClock.databinding.GroupDetailFragmentBinding;
+import org.d3kad3nt.sunriseClock.ui.light.lightDetail.LightDetailFragmentArgs;
+import org.d3kad3nt.sunriseClock.ui.light.lightDetail.LightDetailViewModel;
 import org.d3kad3nt.sunriseClock.ui.util.BaseFragment;
 import org.d3kad3nt.sunriseClock.ui.util.MenuHandler;
+import org.d3kad3nt.sunriseClock.util.LogUtil;
 
 public class GroupDetailFragment extends BaseFragment<GroupDetailFragmentBinding, GroupDetailViewModel> {
-
-    @Nullable
-    @Override
-    protected MenuHandler bindMenu() {
-        return null;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        long groupId = GroupDetailFragmentArgs.fromBundle(requireArguments()).getGroupId();
-        viewModel.loadGroup(groupId);
-
-        binding.swipeRefreshLayout.setOnRefreshListener(() -> viewModel.refreshGroup());
-    }
 
     @Override
     protected GroupDetailFragmentBinding getViewBinding(
@@ -54,12 +46,31 @@ public class GroupDetailFragment extends BaseFragment<GroupDetailFragmentBinding
 
     @Override
     protected ViewModelProvider getViewModelProvider() {
+        long groupId = GroupDetailFragmentArgs.fromBundle(requireArguments()).getGroupId(); // id from navigation
+
+        LogUtil.setPrefix("GroupId %d: ", groupId);
+
+        // We are using a nested navigation graph. From
+        // https://developer.android.com/guide/navigation/use-graph/programmatic#share_ui-related_data_between_destinations_with_viewmodel:
+        // The Navigation back stack stores a NavBackStackEntry not only for each individual
+        // destination, but also for each parent navigation graph that contains the individual
+        // destination.
+        // This allows you to retrieve a NavBackStackEntry that is scoped to a navigation graph.
+        // A navigation graph-scoped NavBackStackEntry provides a way to create a ViewModel that's
+        // scoped to a navigation graph, enabling you to share UI-related data between the graph's
+        // destinations.
+        NavController navController = NavHostFragment.findNavController(this);
+        NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.nav_graph_group_detail);
+
+        // Initialize viewModel with light id and inject the light repository.
+        // By injecting the repository, the viewModel no longer needs the Application or Context.
         MutableCreationExtras viewModelDependencies = new MutableCreationExtras();
         viewModelDependencies.set(
                 GroupDetailViewModel.LIGHT_REPOSITORY_KEY, LightRepository.getInstance(requireContext()));
+        viewModelDependencies.set(GroupDetailViewModel.GROUP_ID_KEY, groupId);
 
         return new ViewModelProvider(
-                this.getViewModelStore(),
+                backStackEntry.getViewModelStore(),
                 ViewModelProvider.Factory.from(GroupDetailViewModel.initializer),
                 viewModelDependencies);
     }
